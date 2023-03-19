01d43f199bb6051006554bf7a231f896e126d7c3
Prateek Aghi <prateekaghi42@gmail.com>
2023-03-19 14:28:50 +0530

    .

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,44 @@
 import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Button from "../components/Common/Button";
 
 const GameScreen = () => {
-  return <Text>game screen</Text>;
+  return (
+    <View style={style.container}>
+      <Text style={style.guess}>Opponents Guess</Text>
+      <View style={style.optionsContainer}>
+        <Text style={style.optionsText}>Higher or Lower</Text>
+        <View style={style.buttonContainer}>
+          <Button>Higher</Button>
+          <Button>Lower</Button>
+        </View>
+      </View>
+    </View>
+  );
 };
-
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    paddingHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 50,
+  },
+  optionsContainer: {
+    backgroundColor: "#72063c",
+    borderRadius: 10,
+    margin: 25,
+  },
+  optionsText: {
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: 700,
+    padding: 20,
+    color: "yellow",
+  },
+  guess: {},
+});
 export default GameScreen;
